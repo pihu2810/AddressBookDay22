1084e5d21fba130727abad88f1a67c412cf51a57
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-19 08:56:32 +0530

    add Address Book

---
+++ src/main/java/com/AddressBook2/Contacts.java
@@ -0,0 +1,39 @@
+package com.AddressBook2;
+
+public class Contacts 
+{
+	
+	    private String firstName;
+	    private String email;
+	    private String phoneNumber;
+
+	    public String getFirstName() {
+	        return firstName;
+	    }
+
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getPhoneNumber() {
+	        return phoneNumber;
+	    }
+
+	    public void setPhoneNumber(String phoneNumber) {
+	        this.phoneNumber = phoneNumber;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Contacts \n[\n\tfirstName=" + firstName + ",\n\temail=" + email + ",\n\tphoneNumber=" + phoneNumber
+	                + "\n]";
+	    }
+	}
